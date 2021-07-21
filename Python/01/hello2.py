67218135a65300ca35db2090ac5635c15ee68309
johnnybigoo <jotalico@icloud.com>
2021-07-21 04:05:58 +0000

    Lecture-Python-4

---
+++ Python/01/hello2.py
@@ -0,0 +1,7 @@
+# get_string and print, with format strings
+
+from cs50 import get_string
+
+answer  = get_string("What's your name? ")
+
+print(f"hello, {answer}")
---
+++ Python/01/hello3.py
@@ -0,0 +1,5 @@
+# input and print, with format strings
+
+answer = input("What's your name? ")
+
+print(f"hello, {answer}")
