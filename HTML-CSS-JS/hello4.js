defcbd8a64bb5b07163f893364291e8b59a08d94
johnnybigoo <jotalico@icloud.com>
2021-07-27 01:33:13 +0000

    Lecture-HTML-CSS-JS-4

---
+++ HTML-CSS-JS/hello4.js
@@ -0,0 +1,8 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('form').onsubmit = function() {
+        alert('hello, ' + document.querySelector('#name').value);
+        return false;
+    };
+
+});
